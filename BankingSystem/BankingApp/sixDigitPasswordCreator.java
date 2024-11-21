c09d244ef2805d35ac6102f285dbe98f6c12af23
Emirhan Yavuz <120987105+emirhanyavuzz@users.noreply.github.com>
2024-11-21 15:03:38 +0300

    Create sixDigitPasswordCreator.java
    sixDigitPasswordCreator class added

---
+++ BankingSystem/BankingApp/sixDigitPasswordCreator.java
@@ -0,0 +1,34 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+public class RandomPasswordToFile {
+    public static void main(String[] args) {
+        Set<String> passwords = new HashSet<>();
+        Random random = new Random();
+
+        while (passwords.size() < 100) {
+            StringBuilder password = new StringBuilder();
+
+            for (int i = 0; i < 6; i++) {
+                int digit = random.nextInt(10); 
+                password.append(digit);
+            }
+
+            passwords.add(password.toString());
+        }
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("password.txt"))) {
+            for (String password : passwords) {
+                writer.write(password);
+                writer.newLine(); 
+            }
+            System.out.println("Şifreler 'password.txt' dosyasına başarıyla yazıldı!");
+        } catch (IOException e) {
+            System.err.println("Dosya yazılırken bir hata oluştu: " + e.getMessage());
+        }
+    }
+}
