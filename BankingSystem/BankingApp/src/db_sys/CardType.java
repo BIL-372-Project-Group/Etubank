49e1af7bf8511662a56912dbc5e79dbc3765e369
Emirhan Yavuz <120987105+emirhanyavuzz@users.noreply.github.com>
2024-11-29 16:34:37 +0300

    Update CardType.java
    updated CardType.java class

--- BankingSystem/BankingApp/src/db_sys/CardType.java
+++ BankingSystem/BankingApp/src/db_sys/CardType.java
@@ -1,44 +1,13 @@
-package db_sys;
+public class CardType {
+    private int cardTypeId; // Primary Key
+    private String typeName; // Card type name (e.g., Credit, Debit)
+    private String description; // Detailed description (Optional)
 
-import java.util.Date;
 
-public class Card {
-    private int cardId; // Primary Key
-    private int accountId; // Foreign Key (references Account.account_id)
-    private int cardTypeId; // Foreign Key (references Card_type.card_type_id)
-    private String cardNumber; // Unique card number
-    private double cardLimit; // Credit/debit limit
-    private Date expiryDate; // Expiry date
-    private int cvv; // Security code
-    private int bankId; // Foreign Key (references Bank.bank_id)
-
-    public Card(int cardId, int accountId, int cardTypeId, String cardNumber, double cardLimit,
-                Date expiryDate, int cvv, int bankId) {
-        this.cardId = cardId;
-        this.accountId = accountId;
+        public CardType(int cardTypeId, String typeName, String description) {
         this.cardTypeId = cardTypeId;
-        this.cardNumber = cardNumber;
-        this.cardLimit = cardLimit;
-        this.expiryDate = expiryDate;
-        this.cvv = cvv;
-        this.bankId = bankId;
-    }
-
-    // Getters and Setters
-    public int getCardId() {
-        return cardId;
-    }
-
-    public void setCardId(int cardId) {
-        this.cardId = cardId;
-    }
-
-    public int getAccountId() {
-        return accountId;
-    }
-
-    public void setAccountId(int accountId) {
-        this.accountId = accountId;
+        this.typeName = typeName;
+        this.description = description;
     }
 
     public int getCardTypeId() {
@@ -49,57 +18,28 @@ public class Card {
         this.cardTypeId = cardTypeId;
     }
 
-    public String getCardNumber() {
-        return cardNumber;
+    public String getTypeName() {
+        return typeName;
     }
 
-    public void setCardNumber(String cardNumber) {
-        this.cardNumber = cardNumber;
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
     }
 
-    public double getCardLimit() {
-        return cardLimit;
+    public String getDescription() {
+        return description;
     }
 
-    public void setCardLimit(double cardLimit) {
-        this.cardLimit = cardLimit;
-    }
-
-    public Date getExpiryDate() {
-        return expiryDate;
-    }
-
-    public void setExpiryDate(Date expiryDate) {
-        this.expiryDate = expiryDate;
-    }
-
-    public int getCvv() {
-        return cvv;
-    }
-
-    public void setCvv(int cvv) {
-        this.cvv = cvv;
-    }
-
-    public int getBankId() {
-        return bankId;
-    }
-
-    public void setBankId(int bankId) {
-        this.bankId = bankId;
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     @Override
     public String toString() {
-        return "Card{" +
-                "cardId=" + cardId +
-                ", accountId=" + accountId +
-                ", cardTypeId=" + cardTypeId +
-                ", cardNumber='" + cardNumber + '\'' +
-                ", cardLimit=" + cardLimit +
-                ", expiryDate=" + expiryDate +
-                ", cvv=" + cvv +
-                ", bankId=" + bankId +
+        return "CardType{" +
+                "cardTypeId=" + cardTypeId +
+                ", typeName='" + typeName + '\'' +
+                ", description='" + description + '\'' +
                 '}';
     }
 }
